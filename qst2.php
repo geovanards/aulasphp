1d2eedf39c322283b1682321bd49db335622aa2d
Geovana Rodrigues <159202999+geovanards@users.noreply.github.com>
2024-09-26 15:40:35 -0300

    Create qst2.php

---
+++ qst2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <form action="processa.php" method="POST">
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome" required>
+        <br>
+        <label for="idade">Ano de Nascimento</label>
+        <input type="text" id="ano_nasc" name="ano_nasc" required>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
+
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>
